40725423cab9530b4580e153efb8eb885c127875
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-09-15 17:50:00 +0530

    Update __main__.py

--- kabeer/__main__.py
+++ kabeer/__main__.py
@@ -3,5 +3,5 @@ from kabeer import app, LOGGER
 from kabeer.modules import *
 import os
 
-print("Pyrogram User Client is Starting... \nPowered By @TheCodents\n(C) 2021 Jayant Kageri" end="\n\n")
+print("Pyrogram User Client is Starting... \nPowered By @TheCodents\n(C) 2021 Jayant Kageri\n" )
 app.run()
