50244787b0c770fcbe9f156ccaf103829f074e39
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-09-15 19:07:09 +0530

    Create filters.py

---
+++ kabeer/helpers/filters.py
@@ -0,0 +1,8 @@
+from typing import List, Union
+from pyrogram import filters
+from config import PREFIXE
+
+other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+other_filters2 = (
+    filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+)
