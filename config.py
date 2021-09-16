814602590296b2e79a365463146a37f3797d437b
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-09-16 20:29:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,5 @@
 import os
 
-__all__ = ['Config']
-
-
-class Config:
-    API_HASH = os.getenv("API_HASH")
-    API_ID = int(os.getenv("API_ID"))
-    HEROKU_API = os.getenv("HEROKU_API")
-    HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
-    PY_SESSION = os.getenv("PYROGRAM_SESSION")
-    PREFIX = os.environ.get("PREFIX", ".")
-    LOG_CHAT = int(os.getenv("LOG_CHAT"))
-    CUSTOM_PACK_NAME = os.environ.get("CUSTOM_PACK_NAME")
 
 
  API_HASH = os.getenv("API_HASH")
